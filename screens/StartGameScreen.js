1ec26b1fe2012940175f560bc5e37fbb1ad3e07b
twój login do GitHuba <twj email>
2023-02-14 11:19:12 +0100

    styling buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,12 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -14,17 +19,28 @@ function StartGameScreen() {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        // flex: 1,
-        marginHorizontal: 24,
-        borderRadius: 8,
-        elevation: 5,
-        shadowColor: "black",
-        shadowOffset: {width: 0, height: 4},
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-        padding: 16,
-        marginTop: 100,
-        backgroundColor: '#72063c',
-    }
-})
+  inputContainer: {
+    // flex: 1,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 5,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#4e0329",
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
